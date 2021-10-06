0ab8a6d9d648f169bed12f21d51dcf61b4d32d41
KT-250r <c011921705@edu.teu.ac.jp>
2021-10-06 00:15:20 -0700

    add dbfunctions

--- code/main.go
+++ code/main.go
@@ -1,35 +1,87 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/jinzhu/gorm"
-    "github.com/mattn/go-sqlite3"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
 type Todo struct {
-    gorm.Model
-    Text string
-    Status string
+	gorm.Model
+	Text   string
+	Status string
 }
 
-finc dbInit(){
-    db, err := gorm.Open("sqlite3", "test.sqliete3")
-    if err != nil {
-        panic("DB can't open..!")
-    }
-    db.AutoMigrate(&Todo{})
-    defer db.Close()
+//DB init
+func dbInit() {
+	db, err := gorm.Open("sqlite3", "test.sqliete3")
+	if err != nil {
+		panic("DB cant open..!(dbInit)")
+	}
+	db.AutoMigrate(&Todo{})
+	defer db.Close()
+}
+
+//DB add
+func dbInsert(text string, status string) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("DB cant open...!(dbInsert)")
+	}
+	db.Create(&Todo{Text: text, Status: status})
+	defer db.Close()
+}
+
+//DB update
+func dbUpdate(id int, text string, status string) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("DB cant open...!")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	db.Save(&todo)
+	db.Close()
+}
+
+//DB delete
+func dbDelete(id, int) {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("DB cant open...!(dbDelete)")
+	}
+	var todo Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
+	db.Close()
+}
+
+//DB get all
+func dbGetAll() []Todo {
+	db, err := gorm.Open("sqlite3", "test.sqlite3")
+	if err != nil {
+		panic("DB cant open...!(dbGetAll())")
+	}
+	var todos []Todo
+	db.order("created_at_desc").Find(&todos)
+	db.Close()
+	return todos
+}
+
+//DB get
+func dbGetOne(id int) Todo {
+    db, err != gorm.Open("sqlite3", "test.sqlite3")
 }
 
 func main() {
-    data := "Hello Go/Gin!"
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+	data := "Hello Go/Gin!"
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
-    router.GET("/", func(ctx *gin.Context){
-	    ctx.HTML(200, "index.html", gin.H{"data": data})
-    })
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+	})
 
-    router.Run()
+	router.Run()
 }
-
